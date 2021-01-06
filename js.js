d25bc4dec91566a352f8576a0822cfc8f30f5cee
pereverzev_daniil <pereverzev.codedot@gmail.com>
2021-01-06 13:11:15 +0300

    Реализация карточек с паралаксом

--- js.js
+++ js.js
@@ -1,3 +1,37 @@
+const paralaxWrap = Array.from(document.body.getElementsByClassName("paralax-inner"));
+const paralaxImg = Array.from(document.body.getElementsByClassName("paralax-img"));
+
+paralaxWrap.forEach((element) => {
+    const card = element.querySelector(".card-wrapper");
+    const cardBg = element.querySelector(".paralax-img");
+    let TimerId;
+
+    element.onmousemove = ((e, element) => {
+        getCurCoordsInsideRect(e, element);
+    })
+
+    const mouseleave = (() => {
+        TimerId = setTimeout(ParalaxStop, 4000)
+    })
+
+
+    const ParalaxStop = (() => {
+        card.style.transform = `rotateY(0deg) rotateX(0deg)`
+        cardBg.style.transform = `translate(0px,0px)  scale(1.4)`
+    })
+
+    element.addEventListener("mouseleave", mouseleave);
+
+
+    const getCurCoordsInsideRect = ((e) => {
+        clearTimeout(TimerId);
+        let x = e.offsetX == undefined ? e.layerX : e.offsetX;
+        let y = e.offsetY == undefined ? e.layerY : e.offsetY;
+        card.style.transform = `rotateY(${((x - 160) / 7)}deg) rotateX(${((y - 205) / 7) * -1}deg)`
+        cardBg.style.transform = `translate(${((x - 200) / 5) * -1}px,${((y - 175) / 5)* -1}px)  scale(1.4)`
+    })
+})
+
 const rotateCard = ((event) => {
     const target = event.target;
     if (target.classList.contains("card-wrapper")) {
