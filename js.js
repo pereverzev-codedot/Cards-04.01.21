46c63cb4f11e149eb8c0272f84303cdc8cd67449
pereverzev_daniil <pereverzev.codedot@gmail.com>
2021-01-12 11:40:43 +0300

    Working with two cards

--- js.js
+++ js.js
@@ -3,10 +3,16 @@ const paralaxImg = Array.from(document.body.getElementsByClassName("paralax-img"
 
 paralaxWrap.forEach((element) => {
     const card = element.querySelector(".card-wrapper");
+    const cardWrap = element.querySelector(".paralax-wrap");
+    const cardWrapBack = element.querySelector(".paralax-wrap-back");
     const cardBg = element.querySelector(".paralax-img");
+    const cardBackBG = element.querySelector(".paralax-img-back");
+
 
     let TimerId;
     const doFlip = () => card.classList.toggle("card-wrapper-rotate")
+
+
     element.addEventListener("click", doFlip);
 
     element.onmousemove = ((e, element) => {
@@ -14,10 +20,10 @@ paralaxWrap.forEach((element) => {
     })
 
     element.onclick = () => {
+        cardWrap.classList.toggle("paralax-wrap-hide")
+        cardWrapBack.classList.toggle("paralax-wrap-hide")
         if (card.classList.contains("card-wrapper-rotate")) {
             card.style.transform = `rotateY(0deg) rotateX(180deg)`
-            cardBg.style.transform = `translate(0px,0px)  scale(1.4) rotateX(-180deg)`
-            card.classList.toggle("paralax-wrap-back")
         } else {
             card.style.transform = `rotateY(0deg) rotateX(0deg)`
             cardBg.style.transform = `translate(0px,0px)  scale(1.4) rotateX(0deg)`
@@ -33,7 +39,6 @@ paralaxWrap.forEach((element) => {
     const ParalaxStop = () => {
         if (card.classList.contains("card-wrapper-rotate")) {
             card.style.transform = `rotateY(0deg) rotateX(180deg)`
-            cardBg.style.transform = `translate(0px,0px)  scale(1.4) rotateX(-180)`
         } else {
             card.style.transform = `rotateY(0deg) rotateX(0deg)`
             cardBg.style.transform = `translate(0px,0px)  scale(1.4) rotateX(0deg)`
@@ -41,7 +46,10 @@ paralaxWrap.forEach((element) => {
 
     }
 
+
+
     element.addEventListener("mouseleave", mouseleave);
+
 
 
     const getCurCoordsInsideRect = (e) => {
@@ -49,7 +57,7 @@ paralaxWrap.forEach((element) => {
         let x = e.offsetX == undefined ? e.layerX : e.offsetX;
         let y = e.offsetY == undefined ? e.layerY : e.offsetY;
         if (card.classList.contains("card-wrapper-rotate")) {
-            cardBg.style.transform = `translate(${((x - 200) / 5) * -1}px,${((y - 175) / 5) * -1}px) rotateX(-180deg) scale(1.4)`
+            cardBackBG.style.transform = `translate(${((x - 200) / 5) * -1}px,${((y - 175) / 5) * -1}px) scale(1.4)`
             card.style.transform = `rotateY(${((x - 160) / 7)}deg) rotateX(${180+((y - 205) / 7) * -1}deg)`
         } else {
             cardBg.style.transform = `translate(${((x - 200) / 5) * -1}px,${((y - 175) / 5) * -1}px)  scale(1.4)`
